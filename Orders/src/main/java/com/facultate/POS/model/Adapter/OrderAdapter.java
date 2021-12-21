291f7f74f9a60f2ba0d71c62658a1230e4b5cb2b
Vlad Paraschiv <vladparaschiv03@gmail.com>
2021-12-21 17:02:36 +0200

    Update OrderAdapter

--- Orders/src/main/java/com/facultate/POS/model/Adapter/OrderAdapter.java
+++ Orders/src/main/java/com/facultate/POS/model/Adapter/OrderAdapter.java
@@ -2,7 +2,9 @@ package com.facultate.POS.model.Adapter;
 
 import com.facultate.POS.model.DTO.OrderDTO;
 import com.facultate.POS.repository.Order.Order;
+import lombok.experimental.UtilityClass;
 
+@UtilityClass
 public class OrderAdapter {
 
     public static Order fromDTO(OrderDTO orderDTO) {
@@ -11,6 +13,15 @@ public class OrderAdapter {
                 .date(orderDTO.getDate())
                 .items(orderDTO.getItems())
                 .status(orderDTO.getStatus())
+                .build();
+    }
+
+    public static OrderDTO toDTO(Order order) {
+        return OrderDTO.builder()
+                .id(order.getId())
+                .date(order.getDate())
+                .items(order.getItems())
+                .status(order.getStatus())
                 .build();
     }
 }
