9724d23ee78e96d001b061b7dd5c07e8c3221783
Vlad Paraschiv <vladparaschiv03@gmail.com>
2021-12-22 10:07:57 +0200

    Current state JWT

---
+++ JWT/src/main/java/com/pos/stateless/WebServiceConfig.java
@@ -0,0 +1,72 @@
+package com.pos.stateless;
+
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.ws.config.annotation.EnableWs;
+import org.springframework.ws.transport.http.MessageDispatcherServlet;
+import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
+import org.springframework.xml.xsd.SimpleXsdSchema;
+import org.springframework.xml.xsd.XsdSchema;
+
+@EnableWs
+@Configuration
+public class WebServiceConfig {
+    @SuppressWarnings({"unchecked", "rawtypes"})
+    @Bean
+    public ServletRegistrationBean messageDispatcherServletCalculator(ApplicationContext applicationContext) {
+        MessageDispatcherServlet result = new MessageDispatcherServlet();
+
+        result.setApplicationContext(applicationContext);
+        result.setTransformWsdlLocations(true);
+
+        return new ServletRegistrationBean(result, "/sample");
+    }
+
+    @SuppressWarnings({"unchecked", "rawtypes"})
+    @Bean
+    public ServletRegistrationBean messageDispatcherServletData(ApplicationContext applicationContext) {
+        MessageDispatcherServlet result = new MessageDispatcherServlet();
+
+        result.setApplicationContext(applicationContext);
+        result.setTransformWsdlLocations(true);
+
+        return new ServletRegistrationBean(result, "/data");
+    }
+
+    @Bean(name = "calculator")
+    public DefaultWsdl11Definition defaultWsdl11DefinitionCalculator(XsdSchema calculatorSchema) {
+        DefaultWsdl11Definition result = new DefaultWsdl11Definition();
+
+        result.setPortTypeName("CalculatorPort");
+        result.setLocationUri("/sample");
+        result.setTargetNamespace("http://com.pos.stateless/calculator");
+        result.setSchema((calculatorSchema));
+
+        return result;
+    }
+
+    @Bean(name = "data")
+    public DefaultWsdl11Definition defaultWsdl11DefinitionData(XsdSchema dataSchema) {
+        DefaultWsdl11Definition result = new DefaultWsdl11Definition();
+
+        result.setPortTypeName("DataPort");
+        result.setLocationUri("/data");
+        result.setTargetNamespace("http://com.pos.stateless/data");
+        result.setSchema((dataSchema));
+
+        return result;
+    }
+
+    @Bean
+    public XsdSchema calculatorSchema() {
+        return new SimpleXsdSchema(new ClassPathResource("Calculator.xsd"));
+    }
+
+    @Bean
+    public XsdSchema dataSchema() {
+        return new SimpleXsdSchema(new ClassPathResource("Data.xsd"));
+    }
+}
---
+++ JWT/src/main/java/com/pos/stateless/repostitory/Data.java
@@ -0,0 +1,27 @@
+package com.pos.stateless.repostitory;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@lombok.Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "Data")
+@Table(name = "data")
+public class Data {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "integer_value")
+    private Integer integerValue;
+
+    @Column(name = "double_value")
+    private Double doubleValue;
+}
