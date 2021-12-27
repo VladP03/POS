e640d3c5c7872ad93b381730ce9b994dbe13ddca
Vlad Paraschiv <vladparaschiv03@gmail.com>
2021-12-27 11:16:05 +0200

    Preserving order in response body

--- Orders/src/main/java/com/facultate/POS/model/DTO/OrderDTO.java
+++ Orders/src/main/java/com/facultate/POS/model/DTO/OrderDTO.java
@@ -3,6 +3,7 @@ package com.facultate.POS.model.DTO;
 import com.facultate.POS.model.Book;
 import com.facultate.POS.model.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Data;
@@ -15,6 +16,7 @@ import java.util.List;
 
 @Data
 @Builder
+@JsonPropertyOrder({"id", "date", "status", "items"})
 public class OrderDTO {
 
     @NotNull(message = "Order's id can not be null")
