212dfb2fa113fc880ad5bd5e3db98fcd7e9a842c
Vlad Paraschiv <vladparaschiv03@gmail.com>
2022-01-03 14:09:00 +0200

    Update Data.xsd + service for Data

--- Calculator/src/main/java/com/pos/stateless/endpoint/DataEndpoint.java
+++ Calculator/src/main/java/com/pos/stateless/endpoint/DataEndpoint.java
@@ -1,7 +1,6 @@
 package com.pos.stateless.endpoint;
 
-import com.pos.stateless.repostitory.Data;
-import com.pos.stateless.repostitory.DataRepository;
+import com.pos.stateless.service.DataService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
@@ -16,90 +15,39 @@ import stateless.pos.com.data.PostEntityResponse;
 import stateless.pos.com.data.PutEntityRequest;
 import stateless.pos.com.data.PutEntityResponse;
 
-import java.math.BigInteger;
-import java.util.Optional;
-
 @Endpoint
 @RequiredArgsConstructor
 public class DataEndpoint {
 
     private static final String NAMESPACE_URI = "http://com.pos.stateless/data";
 
-    private final DataRepository dataRepository;
+    private final DataService dataService;
 
 
     @PayloadRoot(namespace = NAMESPACE_URI, localPart = "postEntityRequest")
     @ResponsePayload
     public PostEntityResponse postEntity(@RequestPayload PostEntityRequest input) {
-        PostEntityResponse result = new PostEntityResponse();
-
-        Data data = dataRepository.save(Data.builder()
-                .name(input.getName())
-                .doubleValue(input.getDoubleValue())
-                .integerValue(input.getIntegerValue().intValue())
-                .build());
-
-        result.setID(data.getId());
-        result.setName(data.getName());
-        result.setDoubleValue(data.getDoubleValue());
-        result.setIntegerValue(BigInteger.valueOf(data.getIntegerValue()));
-
-        return result;
+        return dataService.postEntity(input);
     }
 
 
     @PayloadRoot(namespace = NAMESPACE_URI, localPart = "putEntityRequest")
     @ResponsePayload
     public PutEntityResponse putEntity(@RequestPayload PutEntityRequest input) {
-        PutEntityResponse result = new PutEntityResponse();
-
-        Data data = dataRepository.save(Data.builder()
-                .id(input.getID())
-                .name(input.getName())
-                .doubleValue(input.getDoubleValue())
-                .integerValue(input.getIntegerValue().intValue())
-                .build());
-
-        result.setID(data.getId());
-        result.setName(data.getName());
-        result.setDoubleValue(data.getDoubleValue());
-        result.setIntegerValue(BigInteger.valueOf(data.getIntegerValue()));
-
-        return result;
+        return dataService.putEntity(input);
     }
 
 
     @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getEntityRequest")
     @ResponsePayload
     public GetEntityResponse getEntity(@RequestPayload GetEntityRequest input) {
-        GetEntityResponse result = new GetEntityResponse();
-
-        Optional<Data> data = dataRepository.findById(input.getID());
-
-        if (data.isPresent()) {
-            result.setID(data.get().getId());
-            result.setName(data.get().getName());
-            result.setDoubleValue(data.get().getDoubleValue());
-            result.setIntegerValue(BigInteger.valueOf(data.get().getIntegerValue()));
-
-            return result;
-        }
-
-        throw new RuntimeException("id not found");
+        return dataService.getEntity(input);
     }
 
 
     @PayloadRoot(namespace = NAMESPACE_URI, localPart = "deleteEntityRequest")
     @ResponsePayload
-    public void deleteEntity(@RequestPayload DeleteEntityRequest input) {
-        DeleteEntityResponse result = new DeleteEntityResponse();
-
-        Optional<Data> data = dataRepository.findById(input.getID());
-
-        if (data.isPresent()) {
-            dataRepository.delete(data.get());
-        } else {
-            throw new RuntimeException("id not found");
-        }
+    public DeleteEntityResponse deleteEntity(@RequestPayload DeleteEntityRequest input) {
+        return dataService.deleteEntity(input);
     }
 }
---
+++ Calculator/src/main/java/com/pos/stateless/service/DataService.java
@@ -0,0 +1,97 @@
+package com.pos.stateless.service;
+
+import com.pos.stateless.repostitory.Data;
+import com.pos.stateless.repostitory.DataRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import stateless.pos.com.data.DeleteEntityRequest;
+import stateless.pos.com.data.DeleteEntityResponse;
+import stateless.pos.com.data.GetEntityRequest;
+import stateless.pos.com.data.GetEntityResponse;
+import stateless.pos.com.data.PostEntityRequest;
+import stateless.pos.com.data.PostEntityResponse;
+import stateless.pos.com.data.PutEntityRequest;
+import stateless.pos.com.data.PutEntityResponse;
+
+import java.math.BigInteger;
+import java.util.Optional;
+
+@Component
+@RequiredArgsConstructor
+public class DataService {
+
+    private final DataRepository dataRepository;
+
+
+    public PostEntityResponse postEntity(PostEntityRequest input) {
+        PostEntityResponse result = new PostEntityResponse();
+
+        Data data = dataRepository.save(Data.builder()
+                .name(input.getName())
+                .doubleValue(input.getDoubleValue())
+                .integerValue(input.getIntegerValue().intValue())
+                .build());
+
+        result.setID(data.getId());
+        result.setName(data.getName());
+        result.setDoubleValue(data.getDoubleValue());
+        result.setIntegerValue(BigInteger.valueOf(data.getIntegerValue()));
+
+        result.setResultType("Created");
+
+        return result;
+    }
+
+
+    public PutEntityResponse putEntity(PutEntityRequest input) {
+        PutEntityResponse result = new PutEntityResponse();
+
+        Data data = dataRepository.save(Data.builder()
+                .id(input.getID())
+                .name(input.getName())
+                .doubleValue(input.getDoubleValue())
+                .integerValue(input.getIntegerValue().intValue())
+                .build());
+
+        result.setResultType("No Content");
+
+        return result;
+    }
+
+
+    public GetEntityResponse getEntity(GetEntityRequest input) {
+        GetEntityResponse result = new GetEntityResponse();
+
+        Optional<Data> data = dataRepository.findById(input.getID());
+
+        if (data.isPresent()) {
+            result.setID(data.get().getId());
+            result.setName(data.get().getName());
+            result.setDoubleValue(data.get().getDoubleValue());
+            result.setIntegerValue(BigInteger.valueOf(data.get().getIntegerValue()));
+
+            result.setResultType("Ok");
+
+            return result;
+        }
+
+        throw new RuntimeException("id not found");
+    }
+
+
+    public DeleteEntityResponse deleteEntity(DeleteEntityRequest input) {
+        DeleteEntityResponse deleteEntityResponse = new DeleteEntityResponse();
+
+        Optional<Data> data = dataRepository.findById(input.getID());
+
+        if (data.isPresent()) {
+            dataRepository.delete(data.get());
+
+            deleteEntityResponse.setResultType("No Content");
+
+            return deleteEntityResponse;
+        } else {
+            throw new RuntimeException("id not found");
+        }
+    }
+}
